d0d39e01c896e1c55a62194ac1882b60a27d9f3f
Parkyounhun <yy9976com@naver.com>
2026-03-18 12:27:32 +0900

    hello, Step01, Step02 추가함

---
+++ Step02_Dict.py
@@ -0,0 +1,34 @@
+#dict type 
+
+"""
+- dict type
+1. key:value 형태로 데이터를 저장한다
+2. 순서가 없는 데이터 이다.
+3. key 
+
+"""
+# 회원 1명의 데이터 (list type)
+mem1 = {"num":1, "name":"kimgura", "isMan":True}
+# 회원 1명의 데이터(쓰기 어렵다)
+#info1 = [1, "kimgura", True]
+print(mem1["num"])
+print(mem1["name"])
+print(mem1["isMan"])
+
+# dict 안에 내용을 참조해서 변수에 담기
+a = mem1["num"]
+b = mem1["name"]
+c = mem1["isMan"]
+
+# dict 안에 내용을 수정하기
+mem1["num"] = 2
+mem1["name"] = "park"
+mem1["isMan"] = False
+
+# 특정 key 값으로 저장된 내용 삭제
+del mem1["isMan"]
+
+# 모든 값 삭제
+mem1.clear()
+
+print("종료합니다")
---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+
+print("안녕하세요, 이제 시작 이군요")
